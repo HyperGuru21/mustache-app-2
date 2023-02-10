edd7dda307f35443bf778b0fd0e953c5b357112e
HyperGuru21 <94612217+HyperGuru21@users.noreply.github.com>
2023-02-10 16:55:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preLoad(){
+
+
+}
+
+function setup(){
+
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+
+    
+    video = createCapture(VIDEO);
+    video.size(300 ,300);
+    video.hide();
+
+
+    posenet = ml5.poseNet(video , modelLoaded);
+
+    posenet.on('pose' , getPoses)
+
+}
+
+function modelLoaded(){
+
+console.log("Posenet Intialized");
+
+}
+
+
+function draw(){
+
+image(video , 0 , 0 , 300 , 300);
+
+}
+
+function take_snapshot(){
+
+    save('mymustachefilter.png');
+
+}
+
+function getPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x"+results[0].pose.nose.x);
+        console.log("nose y"+results[0].pose.nose.y);
+    }
+}
